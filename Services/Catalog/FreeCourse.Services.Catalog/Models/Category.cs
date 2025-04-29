41523bb16462a567b276af653f0e69a1a7f90085
akifguldemir <akif.guldemir@ozyer.com>
2025-04-29 14:32:07 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
