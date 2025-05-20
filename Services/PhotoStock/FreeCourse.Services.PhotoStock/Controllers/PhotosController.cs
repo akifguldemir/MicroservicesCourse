66899e4ad9d58445dfe0f53d0750894a308e6eb6
Akif Güldemir <akifguldemir@gmail.com>
2025-05-20 23:39:03 +0300

    Add PhotoStock service and related configurations
    - Updated solution file to include `PhotoStock` and `FreeCourse.Services.PhotoStock`.
    - Added new entry for `photostock` service in `Ports.txt` (port 5012).
    - Created `PhotosController` for managing photo uploads and deletions.
    - Introduced `PhotoDto` for photo data representation.
    - Created project file `FreeCourse.Services.PhotoStock.csproj` with necessary SDK and dependencies.
    - Added HTTP configuration file for service host address and sample requests.
    - Updated `Program.cs` to configure web application middleware.
    - Modified `launchSettings.json` for application URL and environment settings.
    - Created/modified `appsettings.Development.json` and `appsettings.json` for logging configurations.

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,50 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBased;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile file, CancellationToken cancellationToken)
+        {
+            if(file.Length > 0 && file != null)
+            { 
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream, cancellationToken);
+                }
+
+                var returnedPath = Path.Combine("photos", file.FileName);
+
+                PhotoDto photoDto = new()
+                {
+                    Url = returnedPath
+                };
+
+                return CreatedActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+
+            }
+
+            return CreatedActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+
+        }
+
+        public IActionResult Delete(string fileName, CancellationToken cancellationToken)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", fileName);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+                return CreatedActionResultInstance(Response<NoContent>.Success(204));
+            }
+            return CreatedActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+        }
+    }
+}
